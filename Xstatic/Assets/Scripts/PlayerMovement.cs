1ff1a6c6f569f9a6989404c84a1bdd214bc4f69e
Kisame712 <kisamehoshigakiuhmw712@gmail.com>
2025-10-18 00:36:24 +0530

    Added Basic enemy and level

--- Xstatic/Assets/Scripts/PlayerMovement.cs
+++ Xstatic/Assets/Scripts/PlayerMovement.cs
@@ -21,7 +21,6 @@ public class PlayerMovement : MonoBehaviour
     {
         playerInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
         LookForward();
-       
     }
 
     private void FixedUpdate()
@@ -33,7 +32,7 @@ public class PlayerMovement : MonoBehaviour
     private void LookForward()
     {
         Vector3 rotateDirection = new Vector3(playerInput.x, transform.forward.y, playerInput.y);
-        float rotationSpeed = 10f;
+        float rotationSpeed = 2f;
         transform.forward = Vector3.Slerp(transform.forward, rotateDirection, rotationSpeed * Time.deltaTime);
         
     }
